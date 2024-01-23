967fd1347853273ae678a3c888669339c8cf6d28
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-01-23 20:46:52 +0100

    fix phpstan

--- phpstan-baseline.php
+++ phpstan-baseline.php
@@ -7,6 +7,11 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/src/ErrorBag.php',
 ];
 $ignoreErrors[] = [
+	'message' => '#^Strict comparison using \\=\\=\\= between mixed and array\\{\\} will always evaluate to false\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Rules/CreditCard.php',
+];
+$ignoreErrors[] = [
 	'message' => '#^Method Dimtrovich\\\\Validation\\\\Rules\\\\File\\:\\:defaults\\(\\) should return static\\(Dimtrovich\\\\Validation\\\\Rules\\\\File\\)\\|null but return statement is missing\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/src/Rules/File.php',
